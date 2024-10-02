13f1e7999de62df9db6d68f3289b03d465311d2c
Victor Marchi <victor.marchi@sgsistemas.com.br>
2024-10-02 17:55:07 -0300

    Criando Classe da entidade Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Department obj = new Department(1, "Victor");
+		System.out.println(obj);
 	}
 }
