2265e6b5ae4244495ae7e213bfd3b341d2800b86
Victor Marchi <victor.marchi@sgsistemas.com.br>
2024-10-03 11:57:46 -0300

    Criando Classe da entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Victor");
-		System.out.println(obj);
+		Department obj = new Department(1, "TI");
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.print(obj);
+		System.out.println();
+		System.out.print(seller);
 	}
 }
