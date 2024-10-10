09d5d10bb84178e049fa1068ebe02669026d4076
Victor Marchi <victor.marchi@sgsistemas.com.br>
2024-10-10 16:02:25 -0300

    Implementado operacao Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: Seller findById ======");
@@ -34,10 +35,23 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: Seller Insert ======");
+		System.out.println("\n=== TEST 4: Seller Insert ======");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com",new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Seller Update ======");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
+		
+		System.out.println("\n=== TEST 6: Seller Delete ======");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		
+		sc.close();
 
 	}
 }
