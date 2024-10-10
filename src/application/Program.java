54500bcb95ed2e728e931b365f83cb985b1739f4
Victor Marchi <victor.marchi@sgsistemas.com.br>
2024-10-10 13:54:47 -0300

    Implementado operacao findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,14 @@ public class Program {
 		for(Seller obj : list) { 
 			System.out.println(obj);
 		}
-		//System.out.print(seller);
+		
+		System.out.println("\n=== TEST 3: Seller findAll ======");
+
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) { 
+			System.out.println(obj);
+		}
+
 	}
 }
